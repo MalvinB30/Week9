a245589df8a1e6be0a873f2eb3d4575fbd854018
Malv jahanto <MalvinB30>
2025-11-05 11:28:58 +0700

    Interface Animal

---
+++ src/Week09/Animal.java
@@ -0,0 +1,27 @@
+package Week09;
+
+public interface Animal {
+    public void eat();
+    public void travel();
+}
+
+public class Mammal implements Animal{
+
+    public Mammal() {}
+
+    public void eat() {
+        System.out.println("Mammal eats");
+    }
+
+    public void travel() {
+        System.out.println("Mammal travel");
+    }
+
+    public static void main(String[] args) {
+        Mammal m = new Mammal();
+        m.eat();
+        m.travel();
+    }
+
+};
+
